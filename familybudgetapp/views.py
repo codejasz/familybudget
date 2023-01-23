cd25a8a6161625010bb6f2dcdd1d7ac15cd43305
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2023-01-23 23:43:36 +0100

    views.py - addde authentication permision class to all views

--- familybudgetapp/views.py
+++ familybudgetapp/views.py
@@ -1,26 +1,26 @@
-from django.shortcuts import render
-from rest_framework import viewsets, exceptions
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
-from .serializers import UserSerializer, BudgetSerializer, TransactionSerializer
-from .models import User, Budget, Transaction
+
+from .models import Budget, Transaction, User
 from .permissions import IsOwnerOrIsPermitted
+from .serializers import (BudgetSerializer, TransactionSerializer,
+                          UserSerializer)
+
 
 # Create your views here.
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
 
 class BudgetViewSet(viewsets.ModelViewSet):
     serializer_class = BudgetSerializer
     queryset = Budget.objects.all()
     permission_classes = [IsOwnerOrIsPermitted]
 
-    def get_queryset(self):
-        if not self.request.user.is_authenticated:
-            raise exceptions.AuthenticationFailed('Please login to be able to see budget details')
-        return self.request.user.budgets.all()
-
 
 class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = TransactionSerializer
     queryset = Transaction.objects.all()
+    permission_classes = [IsAuthenticated]
