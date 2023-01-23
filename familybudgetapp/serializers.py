e233b6ef075742604522dbad0c19aa9e9d2dd1df
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2023-01-23 23:41:58 +0100

    serializers.py - specify models fields, reformat code, organize imports

--- familybudgetapp/serializers.py
+++ familybudgetapp/serializers.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import User
-from .models import Budget, Transaction
 from rest_framework import serializers
+
+from .models import Budget, Transaction
 
 
 class UserSerializer(serializers.ModelSerializer):
     budgets = serializers.HyperlinkedRelatedField(
-        many=True,
-        read_only=True,
-        view_name='budget-detail'
+        many=True, read_only=True, view_name='budget-detail'
     )
+
     class Meta:
         model = User
         fields = ['url', 'username', 'first_name', 'last_name', 'budgets']
@@ -16,13 +16,22 @@ class UserSerializer(serializers.ModelSerializer):
 
 class BudgetSerializer(serializers.ModelSerializer):
     transactions = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Budget
-        fields = '__all__'
+        fields = ['name', 'description', 'owner', 'transactions']
 
 
 class TransactionSerializer(serializers.ModelSerializer):
     categories = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = Transaction
-        fields = '__all__'
+        fields = [
+            'description',
+            'amount',
+            'transaction_type',
+            'created_at',
+            'category',
+            'budget',
+        ]
