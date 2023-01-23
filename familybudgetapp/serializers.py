27c101ee8c9c2f36e062f9b296ff077f75e7a75a
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2023-01-23 20:11:07 +0100

    serializers.py - added TransactionSerializer, minor changes in other serializers

--- familybudgetapp/serializers.py
+++ familybudgetapp/serializers.py
@@ -1,13 +1,28 @@
 from django.contrib.auth.models import User
-from .models import Budget
+from .models import Budget, Transaction
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    budgets = serializers.HyperlinkedRelatedField(
+        many=True,
+        read_only=True,
+        view_name='budget-detail'
+    )
     class Meta:
         model = User
-        fields = ['firstname', 'lastname', 'email', 'groups']
+        fields = ['url', 'username', 'first_name', 'last_name', 'budgets']
+
 
 class BudgetSerializer(serializers.ModelSerializer):
+    transactions = serializers.StringRelatedField(many=True)
     class Meta:
         model = Budget
+        fields = '__all__'
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    categories = serializers.StringRelatedField(many=True)
+    class Meta:
+        model = Transaction
+        fields = '__all__'
