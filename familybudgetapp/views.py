a3d9694ecf1f4cb2dab32b3849eccc540274a96f
Adrian Kodjasz <adrian.kodjasz@gmail.com>
2023-01-24 12:38:30 +0100

    views.py - overwrite get_queryset method in BudgetViewSet to retrieve only user budgets and user permitted budgets

--- familybudgetapp/views.py
+++ familybudgetapp/views.py
@@ -3,8 +3,12 @@ from rest_framework.permissions import IsAuthenticated
 
 from .models import Budget, Transaction, User
 from .permissions import IsOwnerOrIsPermitted, IsOwner
-from .serializers import (BudgetSerializer, SharedSerializer,
-                          TransactionSerializer, UserSerializer)
+from .serializers import (
+    BudgetSerializer,
+    SharedSerializer,
+    TransactionSerializer,
+    UserSerializer,
+)
 
 
 # Create your views here.
@@ -16,14 +20,20 @@ class UserViewSet(viewsets.ModelViewSet):
 
 class BudgetViewSet(viewsets.ModelViewSet):
     serializer_class = BudgetSerializer
-    queryset = Budget.objects.all()
     permission_classes = [IsOwnerOrIsPermitted]
+
+    def get_queryset(self):
+        user_budgets = self.request.user.budgets.all()
+        user_budgets_permitted = Budget.objects.filter(
+            shared_with__shared_with=self.request.user
+        )
+        return user_budgets | user_budgets_permitted
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
     serializer_class = TransactionSerializer
     queryset = Transaction.objects.all()
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, IsOwnerOrIsPermitted]
 
 
 class SharedViewSet(viewsets.ModelViewSet):
